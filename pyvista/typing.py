36fdaf3cac74676d32c233de4a673730d62e4d8e
JevinJ <jevinjones@openmailbox.org>
2021-01-25 19:08:21 -0800

    Add NumericArray alias

--- pyvista/typing.py
+++ pyvista/typing.py
@@ -1,3 +1,6 @@
-from typing import Union, List, Tuple
+from typing import Union, List, Tuple, Sequence
+
+import numpy as np
 
 Vector = Union[List[float], Tuple[float, float, float]]
+NumericArray = Union[Sequence[Union[float, int]], np.ndarray]
