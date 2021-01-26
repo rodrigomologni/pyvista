c5be1b4f2fcd89161028898268495fcf4a5e0bfd
JevinJ <jevinjones@openmailbox.org>
2021-01-25 21:15:45 -0800

    Add ndarray to Vector alias

--- pyvista/typing.py
+++ pyvista/typing.py
@@ -3,6 +3,6 @@ from typing import Union, List, Tuple, Sequence
 import numpy as np
 
 Number = Union[float, int]
-Vector = Union[List[float], Tuple[float, float, float]]
+Vector = Union[np.ndarray, Union[List[float], Tuple[float, float, float]]]
 NumericArray = Union[Sequence[Number], np.ndarray]
 Color = Union[str, Sequence[Number]]
