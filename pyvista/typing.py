af306db82026a62d1ade21bd224ef851ab342071
JevinJ <jevinjones@openmailbox.org>
2021-01-25 20:20:17 -0800

    Add Number, Color aliases. Improve existing aliases.

--- pyvista/typing.py
+++ pyvista/typing.py
@@ -2,5 +2,7 @@ from typing import Union, List, Tuple, Sequence
 
 import numpy as np
 
+Number = Union[float, int]
 Vector = Union[List[float], Tuple[float, float, float]]
-NumericArray = Union[Sequence[Union[float, int]], np.ndarray]
+NumericArray = Union[Sequence[Number], np.ndarray]
+Color = Union[str, Sequence[Number]]
